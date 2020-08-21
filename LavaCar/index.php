5e56bf5cf77171709a92c57dec7fb50690603274
nath4ns <nathansousa360@gmail.com>
2020-08-20 22:37:53 -0300

    Lava Car

---
+++ LavaCar/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Lava Car</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/estilo.css">
+</head>
+<body>
+	<main>
+		<section class="menu">
+			<div class="imgLogo">
+				<img src="icones/Logo.png">
+			</div>
+			<nav class="menu-superior">
+				<ul>
+					<li>Quem Somos</li>
+					<li>Serviços</li>
+					<li>Agendamento</li>
+					<li>Contato</a></li>
+				</ul>
+			</nav>
+		</section>
+	</main>
+</body>
+</html>
