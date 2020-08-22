fd92164293884816b4e51e14ca80c17d4e7bd797
nath4ns <nathansousa360@gmail.com>
2020-08-21 22:52:56 -0300

    Slide

--- LavaCar/index.php
+++ LavaCar/index.php
@@ -20,6 +20,31 @@
 				</ul>
 			</nav>
 		</section>
+
+
+	<div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="icones/lavagem.jpg" alt="">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="icones/polimento.jpg" alt="">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="icones/higienizacao.jpg" alt="">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="##carouselExampleFade" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Anterior</span>
+  </a>
+  <a class="carousel-control-next" href="##carouselExampleFade" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Próximo</span>
+  </a>
+</div>
+
 	</main>
 </body>
-</html>
+</html> 
+
