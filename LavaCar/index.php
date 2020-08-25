0b7c1b747bb52f4b99a7826182de3175c8462c69
nath4ns <nathansousa360@gmail.com>
2020-08-24 22:24:42 -0300

    Avançando

--- LavaCar/index.php
+++ LavaCar/index.php
@@ -2,49 +2,62 @@
 <html>
 <head>
 	<title>Lava Car</title>
-	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="css/estilo.css">
+	
 </head>
 <body>
 	<main>
-		<section class="menu">
-			<div class="imgLogo">
-				<img src="icones/Logo.png">
-			</div>
-			<nav class="menu-superior">
-				<ul>
-					<li>Quem Somos</li>
-					<li>Serviços</li>
-					<li>Agendamento</li>
-					<li>Contato</li>
-				</ul>
+			<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+			  <img src="icones/Logo.png">
+			  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
+			    <span class="navbar-toggler-icon"></span>
+			  </button>
+
+			  <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
+			    <ul class="navbar-nav mr-auto">
+			      <li class="nav-item">
+			        <a class="nav-link" href="#">Quem Somos</a>
+			      </li>
+			      <li class="nav-item">
+			        <a class="nav-link" href="#">Serviços</a>
+			      </li>
+			      <li class="nav-item">
+			        <a class="nav-link" href="#">Agendamento</a>
+			      </li>
+			      <li class="nav-item">
+			        <a class="nav-link" href="#">Contato</a>
+			      </li>
+			    </ul>
+			  </div>
 			</nav>
-		</section>
 
-
-	<div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img class="d-block w-100" src="icones/lavagem.jpg" alt="">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="icones/polimento.jpg" alt="">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="icones/higienizacao.jpg" alt="">
-    </div>
-  </div>
-  <a class="carousel-control-prev" href="##carouselExampleFade" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Anterior</span>
-  </a>
-  <a class="carousel-control-next" href="##carouselExampleFade" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Próximo</span>
-  </a>
-</div>
-
+			<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+			  <div class="carousel-inner">
+			    <div class="carousel-item active">
+			      <img class="d-block w-100" src="icones/lavagem.jpg" alt="Primeiro Slide">
+			    </div>
+			    <div class="carousel-item">
+			      <img class="d-block w-100" src="icones/higienizacao.jpg" alt="Primeiro Slide">
+			    </div>
+			    <div class="carousel-item">
+			      <img class="d-block w-100" src="icones/polimento.jpg" alt="Primeiro Slide">
+			    </div>
+			  </div>
+			  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+			    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+			    <span class="sr-only">Anterior</span>
+			  </a>
+			  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+			    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+			    <span class="sr-only">Próximo</span>
+			  </a>
+			</div>
 	</main>
+
+<script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/owl.carousel.min.js"></script>
 </body>
 </html> 
 
