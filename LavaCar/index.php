20b317500a8574ab8d04772931fa359a3b4d5503
nath4ns <nathansousa360@gmail.com>
2020-09-22 21:45:23 -0300

    Correção

--- LavaCar/index.php
+++ LavaCar/index.php
@@ -26,7 +26,7 @@
 			        <a class="nav-link" href="#">Agendamento</a>
 			      </li>
 			      <li class="nav-item">
-			        <a class="nav-link" href="#">Contato</a>
+			        <a class="nav-link" href="#">Contatos</a>
 			      </li>
 			    </ul>
 			  </div>
